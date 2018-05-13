398fd8502c1c58b092705f8ad0bfadf12ade67ad
Eman Moustafa <eman.moustaf.2020.1111@gmail.com>
2018-05-13 09:34:36 +0200

    Nutrition

---
+++ build-nutrition-Desktop_Qt_5_10_0_GCC_64bit-Debug/ui_mainwindow.h
@@ -0,0 +1,208 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.10.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QColumnView>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QPushButton *pushButton;
+    QComboBox *comboBox;
+    QTextEdit *textEdit;
+    QLabel *label;
+    QTextEdit *textEdit_2;
+    QTextEdit *textEdit_3;
+    QLabel *label_2;
+    QLabel *label_3;
+    QColumnView *columnView;
+    QLabel *label_4;
+    QCheckBox *checkBox;
+    QCheckBox *checkBox_2;
+    QCheckBox *checkBox_3;
+    QCheckBox *checkBox_4;
+    QCheckBox *checkBox_5;
+    QCheckBox *checkBox_6;
+    QCheckBox *checkBox_7;
+    QCheckBox *checkBox_8;
+    QCheckBox *checkBox_9;
+    QCheckBox *checkBox_10;
+    QCheckBox *checkBox_11;
+    QCheckBox *checkBox_12;
+    QLabel *label_5;
+    QLabel *label_6;
+    QLabel *label_7;
+    QMenuBar *menuBar;
+    QMenu *menuHI_HI_CAPTAIN;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+    QToolBar *toolBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(631, 300);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(580, 220, 51, 21));
+        comboBox = new QComboBox(centralWidget);
+        comboBox->setObjectName(QStringLiteral("comboBox"));
+        comboBox->setGeometry(QRect(10, 0, 91, 25));
+        textEdit = new QTextEdit(centralWidget);
+        textEdit->setObjectName(QStringLiteral("textEdit"));
+        textEdit->setGeometry(QRect(10, 30, 91, 31));
+        label = new QLabel(centralWidget);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(40, 60, 67, 17));
+        textEdit_2 = new QTextEdit(centralWidget);
+        textEdit_2->setObjectName(QStringLiteral("textEdit_2"));
+        textEdit_2->setGeometry(QRect(10, 80, 91, 31));
+        textEdit_3 = new QTextEdit(centralWidget);
+        textEdit_3->setObjectName(QStringLiteral("textEdit_3"));
+        textEdit_3->setGeometry(QRect(10, 140, 91, 31));
+        label_2 = new QLabel(centralWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setGeometry(QRect(30, 110, 67, 17));
+        label_3 = new QLabel(centralWidget);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setGeometry(QRect(30, 170, 67, 17));
+        columnView = new QColumnView(centralWidget);
+        columnView->setObjectName(QStringLiteral("columnView"));
+        columnView->setGeometry(QRect(150, 0, 381, 192));
+        label_4 = new QLabel(centralWidget);
+        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setGeometry(QRect(240, 10, 67, 17));
+        checkBox = new QCheckBox(centralWidget);
+        checkBox->setObjectName(QStringLiteral("checkBox"));
+        checkBox->setGeometry(QRect(160, 70, 92, 23));
+        checkBox_2 = new QCheckBox(centralWidget);
+        checkBox_2->setObjectName(QStringLiteral("checkBox_2"));
+        checkBox_2->setGeometry(QRect(160, 100, 92, 23));
+        checkBox_3 = new QCheckBox(centralWidget);
+        checkBox_3->setObjectName(QStringLiteral("checkBox_3"));
+        checkBox_3->setGeometry(QRect(160, 130, 92, 23));
+        checkBox_4 = new QCheckBox(centralWidget);
+        checkBox_4->setObjectName(QStringLiteral("checkBox_4"));
+        checkBox_4->setGeometry(QRect(160, 160, 92, 23));
+        checkBox_5 = new QCheckBox(centralWidget);
+        checkBox_5->setObjectName(QStringLiteral("checkBox_5"));
+        checkBox_5->setGeometry(QRect(270, 70, 92, 23));
+        checkBox_6 = new QCheckBox(centralWidget);
+        checkBox_6->setObjectName(QStringLiteral("checkBox_6"));
+        checkBox_6->setGeometry(QRect(270, 100, 92, 23));
+        checkBox_7 = new QCheckBox(centralWidget);
+        checkBox_7->setObjectName(QStringLiteral("checkBox_7"));
+        checkBox_7->setGeometry(QRect(270, 130, 92, 23));
+        checkBox_8 = new QCheckBox(centralWidget);
+        checkBox_8->setObjectName(QStringLiteral("checkBox_8"));
+        checkBox_8->setGeometry(QRect(270, 160, 92, 23));
+        checkBox_9 = new QCheckBox(centralWidget);
+        checkBox_9->setObjectName(QStringLiteral("checkBox_9"));
+        checkBox_9->setGeometry(QRect(400, 70, 92, 23));
+        checkBox_10 = new QCheckBox(centralWidget);
+        checkBox_10->setObjectName(QStringLiteral("checkBox_10"));
+        checkBox_10->setGeometry(QRect(400, 100, 92, 23));
+        checkBox_11 = new QCheckBox(centralWidget);
+        checkBox_11->setObjectName(QStringLiteral("checkBox_11"));
+        checkBox_11->setGeometry(QRect(400, 130, 92, 23));
+        checkBox_12 = new QCheckBox(centralWidget);
+        checkBox_12->setObjectName(QStringLiteral("checkBox_12"));
+        checkBox_12->setGeometry(QRect(400, 160, 92, 23));
+        label_5 = new QLabel(centralWidget);
+        label_5->setObjectName(QStringLiteral("label_5"));
+        label_5->setGeometry(QRect(160, 40, 67, 17));
+        label_6 = new QLabel(centralWidget);
+        label_6->setObjectName(QStringLiteral("label_6"));
+        label_6->setGeometry(QRect(280, 40, 67, 17));
+        label_7 = new QLabel(centralWidget);
+        label_7->setObjectName(QStringLiteral("label_7"));
+        label_7->setGeometry(QRect(390, 40, 67, 17));
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 631, 22));
+        menuHI_HI_CAPTAIN = new QMenu(menuBar);
+        menuHI_HI_CAPTAIN->setObjectName(QStringLiteral("menuHI_HI_CAPTAIN"));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+        toolBar = new QToolBar(MainWindow);
+        toolBar->setObjectName(QStringLiteral("toolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, toolBar);
+
+        menuBar->addAction(menuHI_HI_CAPTAIN->menuAction());
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "HI HI CAPTAIN", nullptr));
+        pushButton->setText(QApplication::translate("MainWindow", "OK", nullptr));
+        label->setText(QApplication::translate("MainWindow", "Age", nullptr));
+        label_2->setText(QApplication::translate("MainWindow", "weight", nullptr));
+        label_3->setText(QApplication::translate("MainWindow", "Height", nullptr));
+        label_4->setText(QApplication::translate("MainWindow", "Breakfast", nullptr));
+        checkBox->setText(QApplication::translate("MainWindow", "CheckBox", nullptr));
+        checkBox_2->setText(QApplication::translate("MainWindow", "CheckBox", nullptr));
+        checkBox_3->setText(QApplication::translate("MainWindow", "CheckBox", nullptr));
+        checkBox_4->setText(QApplication::translate("MainWindow", "CheckBox", nullptr));
+        checkBox_5->setText(QApplication::translate("MainWindow", "CheckBox", nullptr));
+        checkBox_6->setText(QApplication::translate("MainWindow", "CheckBox", nullptr));
+        checkBox_7->setText(QApplication::translate("MainWindow", "CheckBox", nullptr));
+        checkBox_8->setText(QApplication::translate("MainWindow", "CheckBox", nullptr));
+        checkBox_9->setText(QApplication::translate("MainWindow", "CheckBox", nullptr));
+        checkBox_10->setText(QApplication::translate("MainWindow", "CheckBox", nullptr));
+        checkBox_11->setText(QApplication::translate("MainWindow", "CheckBox", nullptr));
+        checkBox_12->setText(QApplication::translate("MainWindow", "CheckBox", nullptr));
+        label_5->setText(QApplication::translate("MainWindow", "   Carb", nullptr));
+        label_6->setText(QApplication::translate("MainWindow", "Protein", nullptr));
+        label_7->setText(QApplication::translate("MainWindow", "      Fat", nullptr));
+        menuHI_HI_CAPTAIN->setTitle(QApplication::translate("MainWindow", "HI HI CAPTAIN", nullptr));
+        toolBar->setWindowTitle(QApplication::translate("MainWindow", "toolBar", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
