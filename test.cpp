afd272691f027962008cc29995efc220ad98074c
Eman Moustafa <eman.moustaf.2020.1111@gmail.com>
2018-05-14 18:20:07 +0200

    knapsack

--- test.cpp
+++ test.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 #include "arrays.hpp"
 #include <array>
-void printAll(std::array<std::array<int, 16>, 16> array,int W, int N)
+void printAll(std::array<std::array<int, 16>, 16> array, int W, int N)
 {
-    std::cout << array[W][N] <<std::endl;
+    for (int i = 0; i <= 16; i++)
+    {
+        for (int j = 0; j <= 16; j++)
+        {
+            std::cout << array[W][N] << std::endl;
+        }
+       std::cout<<" "<< std::endl;
+    }
 }
-std::array<std::array<int, 16>, 16> SetRows_Columns(std::array<std::array<int, 16>, 16>&array, int W, int N)
+std::array<std::array<int, 16>, 16> SetRows_Columns(std::array<std::array<int, 16>, 16> &array, int W, int N)
 {
     int n = 0;
     int w = 0;
     while (w <= W)
     {
         array[0][w] = 0;
-        w = w + 10;
+        w = w + 1;
     }
     while (n <= N)
     {
         array[n][0] = 0;
-        n = n + 10;
+        n = n + 1;
     }
 }
 int main()
@@ -31,8 +38,8 @@ int main()
     map::insert(mapp, "protien",15,17);
     map::printAll(mapp);*/
     std::array<std::array<int, 16>, 16> array;
-    SetRows_Columns(array,1600,16 );
-    printAll(array,1600,16);
+    SetRows_Columns(array, 16, 16);
+    printAll(array, 16, 16);
 
-        return 0;
+    return 0;
 }
